70b3a5cd267464afe9af2c8024d7dc37264a4562
Nicolas Colmenares <47890683+NickEsColR@users.noreply.github.com>
2020-12-03 15:13:46 -0500

    update Enterprise class

--- src/model/Enterprise.java
+++ src/model/Enterprise.java
@@ -11,18 +11,34 @@ public class Enterprise {
 	private GraphB<String,Computer> graphB;
 	private GraphA<String,Computer>  graphA;
 	
-	public Enterprise() {}
+	public Enterprise() {
+		graphB = new GraphB(GraphB.SIMPLE_GRAPH);
+		graphA = new GraphA();		
+	}
 	
 	public void addComputer(String serialNumber, String office, int floor) {
-		
+		Computer c = new Computer(serialNumber,office,floor);
+		graphB.add(serialNumber, c);
+		graphA.add(serialNumber, c);
+	}
+	
+	public void addConection(String serialNumber1, String serialNumber2) {
+	
 	}
 	
 	public void removeComputer(String serialNumber) {
-		
+		graphB.removeVertex(serialNumber);
+		graphA.removeVertex(serialNumber);
+	}
+	
+	public void removeConection(String serialNumber1,String serialNumber2) {
+		graphB.removeEdge(serialNumber1, serialNumber2);
+		graphA.removeEdge(serialNumber1, serialNumber2);
 	}
 	
 	public void searchComputer(String serialNumber) {
-		
+		graphB.search(serialNumber);
+		graphA.search(serialNumber);
 	}
 	
 	public void minimunPath(String serialNumber1, String serialNumber2,char type) {
@@ -30,6 +46,10 @@ public class Enterprise {
 	}
 	
 	public void prim(char type) {
-		
+		if(type==ADYACENT_LIST) {
+			 graphA.prim();
+		}else if(type==MATRIX_LIST) {
+			 graphB.prim();
+		}
 	}
 }
